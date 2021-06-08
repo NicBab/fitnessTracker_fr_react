df19e316d0fc93b8f870e7440faacdecdafc304e
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-06-08 15:34:15 -0500

    update build folder for https

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,9 @@
 export { default as Header } from './Header';
 export { default as Home } from './Home';
-
 export { default as Register } from './Register';
 export { default as Login } from './Login';
-
 export { default as Activities } from './Activities';
-
 export { default as Routines } from './Routines';
 export { default as MyRoutines} from './MyRoutines';
 export { default as RoutineRow } from './RoutineRow'
-
 export { default as Footer } from './Footer';
